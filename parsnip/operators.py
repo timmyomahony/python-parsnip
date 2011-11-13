c2bc6ebb02cccb98aa1ac9cd26750edcd2827790
Timmy <timmy@pastylegs.com>
2011-11-13 03:57:32 +0100

    Initial Commit

---
+++ parsnip/operators.py
@@ -0,0 +1,13 @@
+from base import Webtexter
+
+class VodafoneWebtexter(Webtexter):
+	pass
+	
+class MeteorWebtexter(Webtexter):
+	pass
+	
+class O2Webtexter(Webtexter):
+	pass
+	
+class ThreeWebtexter(Webtexter):
+	pass
---
+++ parsnip/settings/meteor.py
@@ -0,0 +1,5 @@
+from common import *
+
+MAX_MSG_LENGTH = 1000
+LOGIN_POST_URL = "https://www.mymeteor.ie/go/mymeteor-login-manager"
+SEND_POST_URL = "https://www.mymeteor.ie/mymeteorapi/index.cfm"
