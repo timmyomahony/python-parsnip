73c29f3739a170923144578b507dd05628ebb324
Timmy O'Mahony <me@timmyomahony.com>
2012-05-02 23:20:39 +0200

    Tidied up setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import os
-from setuptools import setup, find_packages
-
-from parsnip import VERSION
+from setuptools import setup
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
 readme = f.read()
@@ -9,15 +7,14 @@ f.close()
 
 setup(
     name='python-parsnip',
-    version=".".join(map(str, VERSION)),
+    version="0.0.1",
     description='Parsnip is a basic python interface to allow the sending of web texts via the main 4 Irish mobile operators websites. It is influenced by/based on Cabbage http://cabbagetexter.com/.',
     long_description=readme,
     author="Timmy O'Mahony",
     author_email='me@timmyomahony.com',
     url='https://github.com/timmyomahony/python-parsnip',
-    packages=find_packages(),
-    package_data = {
-        'parsnip': [
-        ],
-    },
+    install_requires=[
+      'lxml', 
+    ],
+    packages=['parsnip'], 
 )
