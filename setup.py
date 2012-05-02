d55dddd72c88921fb8ad004c0819e3746e987254
Timmy O'Mahony <me@timmyomahony.com>
2012-05-02 23:16:00 +0200

    Tidied up dist files

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     description='Parsnip is a basic python interface to allow the sending of web texts via the main 4 Irish mobile operators websites. It is influenced by/based on Cabbage http://cabbagetexter.com/.',
     long_description=readme,
     author="Timmy O'Mahony",
-    author_email='timmy@pastylegs.com',
-    url='https://github.com/pastylegs/python-parsnip',
+    author_email='me@timmyomahony.com',
+    url='https://github.com/timmyomahony/python-parsnip',
     packages=find_packages(),
     package_data = {
         'parsnip': [
